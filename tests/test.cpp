789d615ff483268504f64714205573069d8708a2
Dasha <darazubova79@gmail.com>
2026-04-26 11:30:35 +0300

    add test.cpp

---
+++ tests/test.cpp
@@ -0,0 +1,82 @@
+#include "Account.h"
+#include "Transaction.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class AccountMock : public Account {
+public:
+	AccountMock(int id, int balance) : Account(id, balance) {}
+	MOCK_CONST_METHOD0(GetBalance, int());
+	MOCK_METHOD1(ChangeBalance, void(int diff));
+	MOCK_METHOD0(Lock, void());
+	MOCK_METHOD0(Unlock, void());
+};
+
+class TransactionMock : public Transaction {
+public:
+	MOCK_METHOD3(SaveToDataBase, void(Account& from, Account& to, int sum));
+};
+
+TEST(account, mock){
+	AccountMock a(1,1000);
+	EXPECT_CALL(a, GetBalance()).Times(1);
+	EXPECT_CALL(a, ChangeBalance(testing::_)).Times(2);
+	EXPECT_CALL(a, Lock()).Times(2);
+	EXPECT_CALL(a, Unlock()).Times(1);
+	a.GetBalance();
+	EXPECT_THROW(a.ChangeBalance(1000), std::runtime_error); 
+	a.Lock();
+	a.ChangeBalance(1000);
+	EXPECT_THROW(a.Lock(), std::runtime_error); 
+	a.Unlock();
+}
+
+TEST(account, test) {
+	Account a(1, 1000);
+	EXPECT_EQ(a.id(), 1);
+	EXPECT_EQ(a.GetBalance(), 1000);
+	EXPECT_THROW(a.ChangeBalance(1000), std::runtime_error);
+	EXPECT_NO_THROW(a.Lock());
+	a.ChangeBalance(1000);
+	EXPECT_EQ(a.GetBalance(), 2000);
+	EXPECT_THROW(a.Lock(), std::runtime_error);
+	a.Unlock();
+	EXPECT_NO_THROW(a.Lock()); 
+}
+
+TEST(Transaction, mock) {
+    TransactionMock tr;
+    Account a1(1, 1000); 
+    Account a2(2, 2000);
+    tr.set_fee(250);
+
+    // Ожидаем ровно 1 успешный вызов базы для конкретной строки ниже
+    EXPECT_CALL(tr, SaveToDataBase(testing::Ref(a1), testing::Ref(a2), 500))
+        .Times(1);
+
+    // 1. Не вызывает базу (возвращает false, т.к. 499 < 250*2)
+    tr.Make(a1, a2, 499); 
+
+    // 2. ВЫЗЫВАЕТ базу (Times становится 1)
+    tr.Make(a1, a2, 500); 
+
+    // 3. Чтобы тест не упал от исключений, оборачиваем их
+    EXPECT_THROW(tr.Make(a1, a1, 500), std::logic_error);
+    EXPECT_THROW(tr.Make(a1, a2, 99), std::logic_error);
+    EXPECT_THROW(tr.Make(a1, a2, -500), std::invalid_argument);
+}
+
+TEST(Transaction, test) {
+    Transaction tr;
+    Account a1(1, 1000);
+    Account a2(2, 2000);
+    tr.set_fee(250);
+    EXPECT_FALSE(tr.Make(a1, a2, 499));
+    EXPECT_EQ(a1.GetBalance(), 1000);
+    EXPECT_EQ(a2.GetBalance(), 2000);
+    EXPECT_TRUE(tr.Make(a1, a2, 500));
+    EXPECT_EQ(a2.GetBalance(), 1750);
+    EXPECT_THROW(tr.Make(a1, a1, 500), std::logic_error);
+    EXPECT_THROW(tr.Make(a1, a2, 99), std::logic_error);
+    EXPECT_THROW(tr.Make(a1, a2, -500), std::invalid_argument);
+}
